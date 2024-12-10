2d33d5f00f472a949e904d794fd508f0fb1abdad
ci <ci@banuba.com>
2024-12-10 15:36:48 +0300

    1.14.1-204-g6de1b78ed5

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-199-g2f4e3cf953"
+let bnbPackageVersion: Version = "1.14.1-204-g6de1b78ed5"
 
 let package = Package(
     name: "BNBEyes",
@@ -41,8 +41,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBEyes",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-199-g2f4e3cf953/BNBEyes.zip",
-            checksum: "dc1d6a57ec71458ab52c8005d4b32cf1544b851575ea73906d0d0bbb85074687"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-204-g6de1b78ed5/BNBEyes.zip",
+            checksum: "2a6d68af92d2dc87a2a8b36eefe46c1d61eff1e77963d53fb5165ad4b190bfcd"
         ),
         .target(
             name: "BNBEyes_BNBSdkCore",
